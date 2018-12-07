3fb32db96956ab3fe6a317eaf337cfe132402a1c
Slav Donchev <srd@Slavs-MacBook-Pro.local>
2018-12-07 11:12:11 +0200

    update security group rule - port

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "instance-security-group" {
   name_prefix = "tf-instance-"
 
   ingress = {
-    from_port   = 0
+    from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
